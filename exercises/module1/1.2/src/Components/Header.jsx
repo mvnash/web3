72a6b8007f2378b52ea709588331c63b02b45340
Maxime Vlaminck <maxime.vlaminck@student.vinci.be>
2023-09-15 12:02:52 +0200

    1.1-1.2 : import image

--- exercises/module1/1.2/src/Components/Header.jsx
+++ exercises/module1/1.2/src/Components/Header.jsx
@@ -1,5 +1,12 @@
+import heLogo from "img/LOGOHEVINCI.png";
+
 const Header = (props) => {
-  return <h1>{props.headerTitle}</h1>;
+  return (
+    <>
+      <img src={heLogo} alt="Logo" />
+      <h1>{props.headerTitle}</h1>
+    </>
+  );
 };
 
 export default Header;
